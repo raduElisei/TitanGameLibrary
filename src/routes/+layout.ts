cbd50f0b82e1d995b5a6a3a309b7aad25d141f4c
Radu Elisei <45122923+raduElisei@users.noreply.github.com>
2023-09-08 14:51:50 +0300

    Change Env Names And Use Private Env

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,13 @@
 import { invalidate } from "$app/navigation";
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { NEXT_PUBLIC_SUPABASE_ANON_KEY, NEXT_PUBLIC_SUPABASE_URL } from "$env/static/private";
 import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit";
 
 export const load = async ({ fetch, data, depends }: any) => {
 	depends("supabase:auth");
 
 	const supabase = createSupabaseLoadClient({
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl: NEXT_PUBLIC_SUPABASE_URL,
 		event: { fetch },
 		serverSession: data.session
 	});
